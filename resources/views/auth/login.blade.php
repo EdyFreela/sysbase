77bcbb45a857aa23c6e8b2201292414d71a23244
edyulisses <edyulisses@gmail.com>
2017-08-31 23:31:13 -0300

    Login New Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,24 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-4 col-md-offset-4">
+
+            <div class="login-header text-center">
+                <img src="{{ url('assets/imgs/logo-login.png') }}">
+                <h3>Faça login no Sysbase</h3>
+            </div>
+            
             <div class="panel panel-default">
-                <div class="panel-heading">Acesso</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail</label>
+                            <label for="email" class="col-md-12">E-Mail</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
@@ -25,9 +30,10 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Senha</label>
-
-                            <div class="col-md-6">
+                            <label for="password" class="col-md-6">Senha</label>
+                            <label for="password" class="col-md-6"><a href="{{ url('/password/reset') }}">Esqueceu sua Senha?</a></label>
+                            
+                            <div class="col-md-12">
                                 <input id="password" type="password" class="form-control" name="password">
 
                                 @if ($errors->has('password'))
@@ -49,17 +55,30 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary btn-login">
                                     <i class="fa fa-btn fa-sign-in"></i> Acesso
                                 </button>
 
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">Esqueceu sua Senha?</a>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+
+            <div class="panel panel-default">
+                <div class="panel-body text-center">
+                    Novo no Sysbase? <a href="{{ url('/register') }}">Crie uma Conta?</a>
+                </div>
+            </div>
+
+            <div class="login-footer">
+                <a href="{{ url('/terms') }}">Termos</a>
+                <a href="{{ url('/privacy') }}">Privacidade</a>
+                <a href="{{ url('/security') }}">Segurança</a>
+                <a href="{{ url('/contact') }}">Contate-nos</a>
+            </div>
+
         </div>
     </div>
 </div>
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,58 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sysbase</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" crossorigin="anonymous">
+
+    <style>
+        /* COMMON INTERFACE*/
+        body {
+            font-family: 'Roboto', sans-serif;
+        }
+        .login-header {
+            margin-top: 30px;
+            margin-bottom: 20px;
+        }        
+        h3 {
+            font-weight: 100;
+        }
+        .panel {
+            border-radius: 0px;
+            -webkit-box-shadow: 0px;
+            box-shadow: 0px;
+        }        
+        .btn {
+            border-radius: 0px;
+        }
+        .btn-login{
+            width:100%;
+        }
+        .form-control {
+            border-radius: 0px;
+        }
+        .login-footer {
+            margin-top: 40px;
+            font-size: 12px;
+            text-align: center;
+        }
+        .login-footer a{
+            margin-left:10px;
+            margin-right:10px;
+            color:#777;
+        }
+    </style>
+
+</head>
+<body id="app-layout">
+    <div class="container">
+        @yield('content')
+    </div>
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
