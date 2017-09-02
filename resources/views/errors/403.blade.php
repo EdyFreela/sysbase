3d4795fdb69df81a43ffba234ab3a1991dbc414b
edyulisses <edyulisses@gmail.com>
2017-09-01 21:31:17 -0300

    Layout style 403 Page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
  
 @section('content')
-	<h1>You don't have permission.</h1>
+	<div>
+		<ul class="breadcrumb">
+			<li class="completed"><a href="{{ url('home') }}">Home</a></li>
+			<li><a href="javascript:void(0);">Erro</a></li>
+		</ul>
+	</div>
+	<div class="alert alert-danger" role="alert">
+		<h2><i class="fa fa-exclamation-triangle" aria-hidden="true"></i> 403 - Acesso Negado</h2>
+	</div>
+	<h3>Você não tem permissão para acessar este conteúdo.</h3>
+	<p>Contate o administrador do sistema <a href="mailto:{{ env("SITE_ADMIN_MAIL") }}" target="blank">{{ env("SITE_ADMIN_MAIL") }}</a></p>	
 @endsection
